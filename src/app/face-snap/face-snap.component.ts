a50e8fa21d1ac74ad4072b7611e5c88edabc5fc7
ojoffroy <ojoffroy@gmail.com>
2022-01-06 18:28:42 +0100

    literal types

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,3 +1,4 @@
+import { FaceSnapsService } from './../services/face-snaps.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 
@@ -10,16 +11,18 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
+  constructor(private FaceSnapsService: FaceSnapsService) {}
+
   ngOnInit() {
       this.buttonText = 'Oh Snap !';
   }
 
   onSnap() {
     if (this.buttonText === 'Oh Snap !') {
-      this.faceSnap.snaps++;
+      this.FaceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
       this.buttonText = 'Oops, unSnap !'
     } else {
-      this.faceSnap.snaps--;
+      this.FaceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
       this.buttonText = 'Oh Snap !';
     }
   }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -7,6 +7,7 @@ import { FaceSnap } from "../models/face-snap.model";
 export class FaceSnapsService {
   faceSnaps: FaceSnap[] = [
     {
+      id: 4,
       title: 'iguane',
       description: 'Mon meilleur avatar depuis longtemps !',
       imageUrl: 'https://www.codeur.com/system/user_profiles/avatars/000/506/874/large/avatar.jpg?1638200089',
@@ -15,6 +16,7 @@ export class FaceSnapsService {
       location: 'Toulouse'
     },
     {
+      id: 5,
       title: 'Belle montagne',
       description: 'Un endroit magnifique à voir',
       imageUrl: 'https://cdn.pixabay.com/photo/2018/10/11/07/13/mountains-3739050_960_720.jpg',
@@ -23,6 +25,7 @@ export class FaceSnapsService {
       location: 'Les Pyrénées'
     },
     {
+      id: 6,
       title: 'Un bon dessert',
       description: 'Un regal pour les papilles',
       imageUrl: 'https://c.pxhere.com/images/39/68/f5da084a273d7a4e81add613b26c-1608167.jpg!d',
@@ -33,5 +36,19 @@ export class FaceSnapsService {
 
   getAllFaceSnaps(): FaceSnap[] {
     return this.faceSnaps;
+  }
+
+  getFaceSnapById(faceSnapId: number): FaceSnap {
+    const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+    if (!faceSnap) {
+      throw new Error('FaceSnap not found !');
+    } else {
+      return faceSnap;
+    }
+  }
+
+  snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
+    const faceSnap = this.getFaceSnapById(faceSnapId);
+    snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
   }
 }
