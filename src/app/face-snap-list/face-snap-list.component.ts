235ffd6a15b1878c4c446667b4f6ed24711ee575
ojoffroy <ojoffroy@gmail.com>
2022-01-06 17:41:37 +0100

    create face snaps service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -13,31 +13,6 @@ export class FaceSnapListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.faceSnaps = [
-      {
-        title: 'iguane',
-        description: 'Mon meilleur avatar depuis longtemps !',
-        imageUrl: 'https://www.codeur.com/system/user_profiles/avatars/000/506/874/large/avatar.jpg?1638200089',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Toulouse'
-      },
-      {
-        title: 'Belle montagne',
-        description: 'Un endroit magnifique à voir',
-        imageUrl: 'https://cdn.pixabay.com/photo/2018/10/11/07/13/mountains-3739050_960_720.jpg',
-        createdDate: new Date(),
-        snaps: 0,
-        location: 'Les Pyrénées'
-      },
-      {
-        title: 'Un bon dessert',
-        description: 'Un regal pour les papilles',
-        imageUrl: 'https://c.pxhere.com/images/39/68/f5da084a273d7a4e81add613b26c-1608167.jpg!d',
-        createdDate: new Date(),
-        snaps: 0
-      }
-    ];
   }
 
 }
