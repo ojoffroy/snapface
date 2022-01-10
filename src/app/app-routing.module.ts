f8ae5cf7825c0c335b27a58b642808cda57c3b16
ojoffroy <ojoffroy@gmail.com>
2022-01-10 19:53:30 +0100

    spa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { SingleFaceSnapComponent } from './single-face-snap/single-face-snap.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
+  { path: 'facesnaps/:id', component: SingleFaceSnapComponent },
   { path: 'facesnaps', component: FaceSnapListComponent },
   { path: '', component: LandingPageComponent }
 ];
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,7 @@
 import { FaceSnapsService } from './../services/face-snaps.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-face-snap',
@@ -11,7 +12,8 @@ export class FaceSnapComponent implements OnInit {
   @Input() faceSnap!: FaceSnap;
   buttonText!: string;
 
-  constructor(private FaceSnapsService: FaceSnapsService) {}
+  constructor(private faceSnapsService: FaceSnapsService,
+              private router: Router) {}
 
   ngOnInit() {
       this.buttonText = 'Oh Snap !';
@@ -19,11 +21,15 @@ export class FaceSnapComponent implements OnInit {
 
   onSnap() {
     if (this.buttonText === 'Oh Snap !') {
-      this.FaceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'snap');
       this.buttonText = 'Oops, unSnap !'
     } else {
-      this.FaceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
+      this.faceSnapsService.snapFaceSnapById(this.faceSnap.id, 'unsnap')
       this.buttonText = 'Oh Snap !';
     }
+  }
+
+  onViewFaceSnap() {
+    this.router.navigateByUrl(`facesnaps/${this.faceSnap.id}`);
   }
 }
